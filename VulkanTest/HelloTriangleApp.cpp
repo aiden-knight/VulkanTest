8f1b60cd3202325934a62547bef9a190ef5da3ee
Aiden Knight <me@aidenknight.co.uk>
2024-08-02 11:23:00 +0100

    created vertex struct
    created vertex struct and gave the binding and attributes to the graphics pipeline

--- VulkanTest/HelloTriangleApp.cpp
+++ VulkanTest/HelloTriangleApp.cpp
@@ -467,12 +467,15 @@ void HelloTriangleApp::createGraphicsPipeline() {
     dynamicState.pDynamicStates = dynamicStates.data();
 
     // vertex input
+    auto bindingDesc = Vertex::getBindingDescription();
+    auto attributeDescs = Vertex::getAttributeDescriptions();
+
     VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
     vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
-    vertexInputInfo.vertexBindingDescriptionCount = 0;
-    vertexInputInfo.pVertexBindingDescriptions = nullptr;
-    vertexInputInfo.vertexAttributeDescriptionCount = 0;
-    vertexInputInfo.pVertexAttributeDescriptions = nullptr;
+    vertexInputInfo.vertexBindingDescriptionCount = 1;
+    vertexInputInfo.vertexAttributeDescriptionCount = static_cast<uint32_t>(attributeDescs.size());
+    vertexInputInfo.pVertexBindingDescriptions = &bindingDesc;
+    vertexInputInfo.pVertexAttributeDescriptions = attributeDescs.data();
 
     // input type
     VkPipelineInputAssemblyStateCreateInfo inputAssembly{};
--- VulkanTest/Structures.h
+++ VulkanTest/Structures.h
@@ -11,6 +11,7 @@
 #include <limits>
 #include <algorithm>
 #include <fstream>
+#include <array>
 
 constexpr uint32_t WIDTH = 400;
 constexpr uint32_t HEIGHT = 400;
@@ -65,4 +66,37 @@ struct SwapchainSupportDetails {
     VkSurfaceCapabilitiesKHR capabilities;
     std::vector<VkSurfaceFormatKHR> formats;
     std::vector<VkPresentModeKHR> presentModes;
+};
+
+#include <glm/glm.hpp>
+
+struct Vertex {
+    glm::vec2 pos;
+    glm::vec3 color;
+
+    static VkVertexInputBindingDescription getBindingDescription() {
+        VkVertexInputBindingDescription bindingDesc{};
+
+        bindingDesc.binding = 0;
+        bindingDesc.stride = sizeof(Vertex);
+        bindingDesc.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+        return bindingDesc;
+    }
+
+    static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
+        std::array<VkVertexInputAttributeDescription, 2> attributeDesc{};
+
+        attributeDesc[0].binding = 0;
+        attributeDesc[0].location = 0;
+        attributeDesc[0].format = VK_FORMAT_R32G32_SFLOAT;
+        attributeDesc[0].offset = offsetof(Vertex, pos);
+
+        attributeDesc[1].binding = 0;
+        attributeDesc[1].location = 1;
+        attributeDesc[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDesc[1].offset = offsetof(Vertex, color);
+
+        return attributeDesc;
+    }
 };
